592eaa37f67c12ce75077a4b1483a4ccb8197c2f
Sidarzo <cyprien.rimbaud@orange.fr>
2022-08-12 23:03:28 +0200

    Add skills

--- components/About.vue
+++ components/About.vue
@@ -2,29 +2,29 @@
 <div class="container mx-auto">
         <div class="grid mt-40">      
             <vue-typed-js :strings="['Rimbaud Cyprien']" :showCursor="false" :shuffle="true" :cursorChar="'_'" :typeSpeed="75" >
-                <p class="typing container mx-auto md:px-80 ml-12 mr-12 font-medium md:text-5xl font-sans md:font-serif"></p>
+                <p class="typing container mx-auto md:px-80 ml-8 mr-12 font-medium md:text-5xl font-sans md:font-serif"></p>
             </vue-typed-js>
             <vue-typed-js :strings="['Apprenti développeur']" :showCursor="false" :cursorChar="'_'" :typeSpeed="75" >
-                <p class="typing container mx-auto md:px-80 ml-12 mr-12 font-medium md:text-5xl font-sans md:font-serif"></p>
+                <p class="typing container mx-auto md:px-80 ml-8 mr-12 font-medium md:text-5xl font-sans md:font-serif"></p>
             </vue-typed-js>
             <div class="grid place-content-center item-center mt-10 mb-10">
                 <button v-if="aboutDisplay" v-on:click="removeAbout">
                     <svg class="animate-bounce w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 10l7-7m0 0l7 7m-7-7v18"></path></svg>
+                    <span>Finalement je veux pas savoir...</span>
                 </button>
 
                 <button v-else v-on:click="addAbout">
                 <svg class="animate-bounce w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>
+                    <span class="">En savoir plus sur moi</span>
                 </button>
-                
             </div>
             <div v-if="aboutDisplay">
                 <vue-typed-js :strings="[this.aboutTxt]" :showCursor="false" :cursorChar="'_'" :typeSpeed="5" >
                     <p class="typing container mx-auto ml-12 mr-12 font-medium md:text-xl font-sans md:font-serif text-justify"></p>
                 </vue-typed-js>
             </div>
-
         </div>
-        </div>
+    </div>
         
 </template>
 
--- components/Skills.vue
+++ components/Skills.vue
@@ -1,11 +1,48 @@
 <template>
-<div class="bg-amber-100 h-24">
+<div class="h-full bg-gray-900">
     <div class="container mx-auto">
-        <h1>
-             Skills
-        </h1>
+        <div class="grid grid-rows-3 grid-flow-col gap-4 text-white text-center md:text-justify">
+            <div class="md:row-span-3">
+                <h1 class="text-3xl font-medium">
+                    Compétences & expériences
+                </h1>
+                <p class="text-sm max-w-prose">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis, nibh a cursus tempor, urna est aliquam justo, nec porttitor dolor neque ut lacus. Donec interdum ut ipsum et pulvinar. Aliquam sit amet ullamcorper tortor. Suspendisse ut posuere risus. Morbi pulvinar pharetra diam, non suscipit dui tincidunt ac. Praesent lacinia ex ut turpis lacinia sollicitudin dignissim ut augue. Nullam tincidunt arcu eu risus consequat sollicitudin. Nulla venenatis nisi nec sollicitudin efficitur. Nulla est sapien, aliquam eu leo et, pellentesque tristique lacus. Sed bibendum lorem et turpis laoreet iaculis. Nunc lectus dolor, rutrum quis nisi nec, aliquet convallis justo. Morbi ultrices blandit orci. Nulla id hendrerit tortor. Donec semper ac nunc eget tristique. Aliquam dapibus enim vitae mi mattis, ut pharetra mauris porta.
+                    <br>
+                    Nulla non auctor quam. Donec tempus ac sapien eget auctor. Duis venenatis odio ac orci tincidunt, at elementum urna placerat. Vestibulum dictum efficitur tellus non convallis. Nunc consequat est eu quam ornare facilisis. Maecenas id diam vitae purus ultrices varius. Etiam placerat volutpat ante, bibendum cursus diam sollicitudin sagittis. Etiam tempor posuere nisi, in finibus eros accumsan vitae. Vestibulum elementum eros aliquam, ultrices mi at, rhoncus purus. Duis dictum nibh nec erat fermentum, at pretium tortor convallis. Curabitur viverra ligula eget lorem condimentum, sit amet bibendum justo consequat.
+                </p>
+            </div>
+            <div class="col-span-2"> 
+                <div>
+                    <!-- Laravel -->
+                <p class="font-medium mb-1 mt-3">Laravel</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                </div>
+                    <!-- Flutter -->
+                <p class="font-medium mb-1 mt-3">Flutter</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                </div>
+                    <!-- C# -->
+                <p class="font-medium mb-1 mt-3">C#</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                </div>
+                    <!-- Unity -->
+                <p class="font-medium mb-1 mt-3">Unity</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                </div>
+                    
+            </div>
+            </div>
+            <div class="md:row-span-2 col-span-2">
+                <p>Text</p>
+            </div>
+        </div>
     </div>
-    </div>
+</div>
 </template>
 
 <script>
