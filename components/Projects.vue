266dac3d760580e44c0328a03f6243a4569e6dc7
Sidarzo <cyprien.rimbaud@orange.fr>
2022-08-12 23:50:00 +0200

    Project template

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,11 +1,53 @@
 <template>
-    <div class="">
-        <h1>Projects</h1>
+    <div class="container mx-auto">
+        <h1 class="text-center text-3xl mb-5 mt-4">Projects</h1>
+        <div class="grid grid-rows-3 grid-flow-col gap-1 place-content-center">
+            <div class="relative">
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
 
+        }
+    }
 }
 </script>
--- components/Skills.vue
+++ components/Skills.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="h-full bg-gray-900">
+<div class="h-auto bg-gray-900">
     <div class="container mx-auto">
         <div class="grid grid-rows-3 grid-flow-col gap-4 text-white text-center md:text-justify">
-            <div class="md:row-span-3">
+            <div class="md:row-span-3 mt-5">
                 <h1 class="text-3xl font-medium">
                     Compétences & expériences
                 </h1>
-                <p class="text-sm max-w-prose">
+                <p class="md:text-base text-sm max-w-prose mt-3">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis, nibh a cursus tempor, urna est aliquam justo, nec porttitor dolor neque ut lacus. Donec interdum ut ipsum et pulvinar. Aliquam sit amet ullamcorper tortor. Suspendisse ut posuere risus. Morbi pulvinar pharetra diam, non suscipit dui tincidunt ac. Praesent lacinia ex ut turpis lacinia sollicitudin dignissim ut augue. Nullam tincidunt arcu eu risus consequat sollicitudin. Nulla venenatis nisi nec sollicitudin efficitur. Nulla est sapien, aliquam eu leo et, pellentesque tristique lacus. Sed bibendum lorem et turpis laoreet iaculis. Nunc lectus dolor, rutrum quis nisi nec, aliquet convallis justo. Morbi ultrices blandit orci. Nulla id hendrerit tortor. Donec semper ac nunc eget tristique. Aliquam dapibus enim vitae mi mattis, ut pharetra mauris porta.
                     <br>
                     Nulla non auctor quam. Donec tempus ac sapien eget auctor. Duis venenatis odio ac orci tincidunt, at elementum urna placerat. Vestibulum dictum efficitur tellus non convallis. Nunc consequat est eu quam ornare facilisis. Maecenas id diam vitae purus ultrices varius. Etiam placerat volutpat ante, bibendum cursus diam sollicitudin sagittis. Etiam tempor posuere nisi, in finibus eros accumsan vitae. Vestibulum elementum eros aliquam, ultrices mi at, rhoncus purus. Duis dictum nibh nec erat fermentum, at pretium tortor convallis. Curabitur viverra ligula eget lorem condimentum, sit amet bibendum justo consequat.
@@ -15,29 +15,38 @@
             <div class="col-span-2"> 
                 <div>
                     <!-- Laravel -->
-                <p class="font-medium mb-1 mt-3">Laravel</p>
+                <p class="font-medium mb-1 mt-5">Laravel</p>
                 <div class="col-span-2 rounded-full bg-gray-700 w-full">
-                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 45%"></div>
                 </div>
                     <!-- Flutter -->
                 <p class="font-medium mb-1 mt-3">Flutter</p>
                 <div class="col-span-2 rounded-full bg-gray-700 w-full">
-                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 75%"></div>
                 </div>
                     <!-- C# -->
                 <p class="font-medium mb-1 mt-3">C#</p>
                 <div class="col-span-2 rounded-full bg-gray-700 w-full">
-                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 15%"></div>
                 </div>
                     <!-- Unity -->
                 <p class="font-medium mb-1 mt-3">Unity</p>
                 <div class="col-span-2 rounded-full bg-gray-700 w-full">
-                    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 30%"></div>
                 </div>
-                    
+                      <!-- JS -->
+                <p class="font-medium mb-1 mt-3">JavaScript</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 90%"></div>
+                </div>
+                    <!-- Python -->
+                <p class="font-medium mb-1 mt-3">Python</p>
+                <div class="col-span-2 rounded-full bg-gray-700 w-full">
+                    <div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-full" style="width: 100%"></div>
+                </div>
             </div>
             </div>
-            <div class="md:row-span-2 col-span-2">
+            <div class="md:row-span-1 col-span-2">
                 <p>Text</p>
             </div>
         </div>
