d3846224f6e08406f9fa8525e93ff9080dd4ee16
Sidarzo <cyprien.rimbaud@orange.fr>
2022-08-13 01:00:13 +0200

    Add transition for blur

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,42 +1,42 @@
 <template>
     <div class="container mx-auto">
         <h1 class="text-center text-3xl mb-5 mt-4">Projects</h1>
-        <div class="grid grid-rows-3 grid-flow-col gap-1 place-content-center">
+        <div class="grid md:grid-rows-3 grid-rows-6 grid-flow-col gap-1 place-content-center">
             <div class="relative">
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
             <div>
-                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer" />
+                <img src="https://via.placeholder.com/200" class="hover:blur-sm cursor-pointer ease-in-out duration-500" />
             </div>
         </div>
     </div>
